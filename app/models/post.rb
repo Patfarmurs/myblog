ff8194d2d736a722574b8938057f7a675c1913fd
unknown <patrickkeita80@yahoo.com>
2023-07-15 13:35:54 -0700

    Add validate

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,14 @@
 class Post < ApplicationRecord
+  attribute :posts_count, :integer, default: 0
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :posts_count, comparison: { greater_than_or_equal_to: 0 }
+  attribute :comments_count, :integer, default: 0
+  attribute :likes_count, :integer, default: 0
+  
+  validates :title, presence: true, length: { in: 1..250 }
+  validates :comments_count, comparison: { greater_than_or_equal_to: 0 }
+  validates :likes_count, comparison: { greater_than_or_equal_to: 0 } 
+  
   belongs_to :author, class_name: 'User', foreign_key: 'author_id', counter_cache: true
   has_many :likes
   has_many :comments
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject { User.new(name: 'Abebe', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+
+  before { subject.save }
+
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'posts counter should be greater than or equal to 0' do
+    subject.posts_count = -1
+    expect(subject).to_not be_valid
+  end
+
+  it 'recent comments should be present' do
+    post = Post.new(author: subject, title: 'About Rais Rspec', text: 'Hello')
+    comment = Comment.create(post:, author: subject, text: 'my comment')
+    expect(comment.text).to eq('my comment')
+  end
+
+  it 'recent posts (show_recent_posts) should be present' do
+    Post.create(author: subject, title: 'About Rais Rspec', text: 'Hello')
+    expect(subject.show_recent_posts.length).to eq 1
+  end
+end
